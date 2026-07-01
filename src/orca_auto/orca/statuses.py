0643fe294cc8856d9c3c67774558bed7d6f9c8b5
daehyupsohn <daehyupsohn@users.noreply.github.com>
2026-07-01 20:39:12 +0900

    fix: classify ORCA zero-distance geometry failures

--- src/orca_auto/orca/statuses.py
+++ src/orca_auto/orca/statuses.py
@@ -18,6 +18,7 @@ class AnalyzerStatus(str, Enum):
     ERROR_MULTIPLICITY_IMPOSSIBLE = "error_multiplicity_impossible"
     ERROR_DISK_IO = "error_disk_io"
     ERROR_MEMORY = "error_memory"
+    ERROR_GEOMETRY = "error_geometry"
     GEOM_NOT_CONVERGED = "geom_not_converged"
     TS_NOT_FOUND = "ts_not_found"
     INCOMPLETE = "incomplete"
